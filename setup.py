c3e31e9d803194b443658b79684dd4bdb4772f97
Alistair Lynn <arplynn@gmail.com>
2015-01-29 16:11:42 +0000

    Work around importing dependencies before setup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import find_packages, setup
-from sr.comp.scheduler.metadata import VERSION, DESCRIPTION
+import sr.comp.scheduler.metadata as md
 
 with open('README.rst') as f:
     long_description = f.read()
 
 setup(name='sr.comp.scheduler',
-      version=VERSION,
+      version=md.VERSION,
       packages=find_packages(),
       namespace_packages=['sr', 'sr.comp'],
-      description=DESCRIPTION,
+      description=md.DESCRIPTION,
       long_description=long_description,
       license='MIT',
       author='Student Robotics Competition Software SIG',
--- sr/comp/scheduler/__init__.py
+++ sr/comp/scheduler/__init__.py
@@ -1,2 +1,7 @@
-from .scheduler import Scheduler, max_possible_match_periods, main
+__all__ = ['scheduler', 'metadata']
+
+try:
+    from .scheduler import Scheduler, max_possible_match_periods, main
+except ImportError:
+    pass
 
